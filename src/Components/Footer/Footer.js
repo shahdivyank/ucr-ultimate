106292d220e69a5a692f983c6251c9d0f0aa86e2
shahdivyank <divyanks.shah.2016@gmail.com>
2021-12-17 19:59:23 -0800

    add footer social links, fix hover behavior

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,11 +4,15 @@ import Separator from '../Header/Separator'
 
 function Footer() {
     return (
-        <div className = "footer">
-            <Separator height= "10"/>
-            <p>Contact Us</p> 
-            
-            <p>Copyright &copy; 2021 All Rights Reserved</p>            
+        <div className="footer">
+            <Separator height="10" />
+            <p>Follow us on Instagram
+                <a href="https://instagram.com/ucrultimate" target="_blank" rel="noreferrer"> @ucrultimate </a>
+                and Facebook <a href="https://www.facebook.com/groups/UCRultimate/" target="_blank" rel="noreferrer">@Ultimate Frisbee Riverside</a>
+                !
+            </p>
+
+            <p>Copyright &copy; 2021 All Rights Reserved</p>
         </div>
 
     )
