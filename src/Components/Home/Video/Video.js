4784ead344a294b093ec62fb70240d7fd6c86339
shahdivyank <divyanks.shah.2016@gmail.com>
2021-12-18 01:18:26 -0800

    add poster for video tag

--- src/Components/Home/Video/Video.js
+++ src/Components/Home/Video/Video.js
@@ -5,7 +5,7 @@ import "./Video.css"
 function Video() {
     return (
         <div className='video-container'>
-            <video autoPlay loop muted playsinline>
+            <video autoPlay loop muted playsinline poster="https://www.pe.com/wp-content/uploads/2019/01/RPE-L-RECREATION-0131-01TP.jpg?w=620">
                 <source src= {video} type='video/mp4'/>
             </video>
             <div className='cover'></div>
