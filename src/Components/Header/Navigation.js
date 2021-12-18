8e77129e862875884ae98a5d09534ce94895eee8
shahdivyank <divyanks.shah.2016@gmail.com>
2021-12-17 16:47:56 -0800

    update navbar to sticky

--- src/Components/Header/Navigation.js
+++ src/Components/Header/Navigation.js
@@ -5,20 +5,21 @@ import { HashLink } from 'react-router-hash-link';
 
 function Navigation() {
     return (
-            <Navbar collapseOnSelect expand="md" variant="dark" className="navbar">
-                <Container>
+        <Navbar sticky="top" collapseOnSelect expand="md" variant="dark" className="navbar">
+            <Container>
                 {/* <Navbar.Brand><HashLink smooth to="/#" className='nav-brand navitem'>UCR Ultimate Frisbee</HashLink></Navbar.Brand> */}
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="m-auto">
-                            <HashLink smooth to="/#about-us" className='navitem'>About Us</HashLink>
-                            <HashLink smooth to="/#join-us" className='navitem'>Join Us</HashLink>
-                            <HashLink smooth to="/#announcements" className='navitem'>Announcements</HashLink>
-                            <HashLink smooth to="/#team" className='navitem'>The Team</HashLink>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="m-auto">
+                        <HashLink smooth to="/#" className='navitem'>Home</HashLink>
+                        <HashLink smooth to="/#about-us" className='navitem'>About Us</HashLink>
+                        <HashLink smooth to="/#join-us" className='navitem'>Join Us</HashLink>
+                        <HashLink smooth to="/#announcements" className='navitem'>Announcements</HashLink>
+                        <HashLink smooth to="/#team" className='navitem'>The Team</HashLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
